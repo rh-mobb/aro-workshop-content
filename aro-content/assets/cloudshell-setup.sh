472f209413a0d1f7807cc026f68758d12be90130
tylerstacey <tstacey@redhat.com>
2022-08-30 16:36:36 -0300

    Update setup script

--- aro-content/assets/cloudshell-setup.sh
+++ aro-content/assets/cloudshell-setup.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
 az account set --subscription 'my-subscription-name'
+
+cd ~
+curl https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz > openshift-client-linux.tar.gz
+
+mkdir openshift
+
+tar -zxvf openshift-client-linux.tar.gz -C openshift
+
+echo 'export PATH=$PATH:~/openshift' >> ~/.bashrc && source ~/.bashrc
