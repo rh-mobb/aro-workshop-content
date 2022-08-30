b83c83700a671e2aeddaef5a745b30f654810f14
tylerstacey <tstacey@redhat.com>
2022-08-30 16:47:48 -0300

    Change install script

--- aro-content/assets/cloudshell-setup.sh
+++ aro-content/assets/cloudshell-setup.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-az account set --subscription 'my-subscription-name'
-
 cd ~
 curl https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz > openshift-client-linux.tar.gz
 
 mkdir openshift
+
 tar -zxvf openshift-client-linux.tar.gz -C openshift
+
 mv openshift/oc /usr/local/bin/oc
-chmod +x /usr/local/bin/oc
+echo 'export PATH=$PATH:~/openshift' >> ~/.bashrc
+
+source ~/.bashrc
+
+az account set --subscription 'my-subscription-name'
