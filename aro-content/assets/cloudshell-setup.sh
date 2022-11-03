15ad9f595fa4fd906b500f3e258d8f726f10e4a3
tylerstacey <tstacey@redhat.com>
2022-11-03 14:07:09 -0300

    Fix cloudshell setup

--- aro-content/assets/cloudshell-setup.sh
+++ aro-content/assets/cloudshell-setup.sh
@@ -7,11 +7,7 @@ mkdir openshift
 
 tar -zxvf openshift-client-linux.tar.gz -C openshift
 
-echo 'export PATH=$PATH:~/openshift' >> ~/.bashrc && source ~/.bashrc
-
 oc completion bash > ~/openshift/oc_bash_completion
-
-echo 'source ~/openshift/oc_bash_completion' >> ~/.bashrc && source ~/.bashrc
 
 curl -Ls https://sh.jbang.dev | bash -s - trust add https://repo1.maven.org/maven2/io/quarkus/quarkus-cli/
 curl -Ls https://sh.jbang.dev | bash -s - app install --fresh --force quarkus@quarkusio
@@ -20,17 +16,17 @@ curl -L https://github.com/a8m/envsubst/releases/download/v1.2.0/envsubst-`uname
 chmod +x envsubst
 mkdir envsub
 mv envsubst envsub/
-echo 'export PATH=$PATH:~/envsub' >> ~/.bashrc && source ~/.bashrc
 
 wget https://mirror.openshift.com/pub/openshift-v4/clients/pipeline/latest/tkn-linux-amd64.tar.gz
 mkdir tkn
 tar -xvf tkn-linux-amd64.tar.gz -C tkn 
-echo 'export PATH=$PATH:~/tkn' >> ~/.bashrc && source ~/.bashrc
 
 wget http://download.joedog.org/siege/siege-latest.tar.gz
 mkdir siege
 tar -xvf siege-latest.tar.gz -C siege
-echo 'export PATH=$PATH:~/siege' >> ~/.bashrc && source ~/.bashrc
+
+echo 'export PATH=$PATH:~/siege:~/openshift:~/envsub:~/tkn' >> ~/.bashrc
+source ~/.bashrc
 
 az provider register -n Microsoft.RedHatOpenShift --wait
 az provider register -n Microsoft.Compute --wait
